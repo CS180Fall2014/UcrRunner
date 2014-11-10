b5732c3a457d90520e85d263190d59e20944464b
Martin Ting <mting005@ucr.edu>
2014-11-09 19:30:39 -0800

    fireBase Manager saveRoute Implementation
    fireBase Manager saveRoute is now functional to save routes.

--- app/src/main/java/com/murraycole/ucrrunner/view/FirebaseManager.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/FirebaseManager.java
@@ -1,7 +1,37 @@
 package com.murraycole.ucrrunner.view;
 
+import android.util.Log;
+
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.HashMap;
+
 /**
  * Created by mbrevard on 11/9/14.
  */
 public class FirebaseManager {
+    private Firebase baseRef;
+
+
+    FirebaseManager(){
+        baseRef = getRef();
+    }
+
+    static FirebaseError saveRoute( Route currRoute, String uid){
+        uid = uid.split(":")[1];
+        Log.d("MT", "uid recieved: " + uid);
+        //routeRef to outes/uid/
+        Firebase routesRef = getRef().child("routes").child(uid);
+
+        //set to routes/uid/<genid_currRoute>/currRoute
+        routesRef.push().setValue(currRoute);
+
+        return null;
+    }
+
+    // use this to get baseRef in static functions.
+    static Firebase getRef(){
+        return new Firebase("https://torid-inferno-2246.firebaseio.com/");
+    }
 }

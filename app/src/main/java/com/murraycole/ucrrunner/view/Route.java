7af944a785b9a0b39b955b2ccba6e03895e43b62
mikebrevard <mikebrevard@gmail.com>
2014-11-09 17:20:20 -0800

    Updated route, stats files and firebase manager

--- app/src/main/java/com/murraycole/ucrrunner/view/Route.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/Route.java
@@ -1,7 +1,40 @@
 package com.murraycole.ucrrunner.view;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by mbrevard on 11/9/14.
  */
 public class Route {
+    private List<List<LatLng>> currentRoute;
+    private Stats currentStats;
+
+    Route() {
+        currentRoute = new ArrayList<List<LatLng>>();
+        currentStats = new Stats();
+    }
+
+    Route(List<List<LatLng>> route, Stats stats) {
+        currentRoute = route;
+        currentStats = stats;
+    }
+
+    public void setCurrentRoute(List<List<LatLng>> route) {
+        currentRoute = route;
+    }
+
+    public void setCurrentStats(Stats stats) {
+        currentStats = stats;
+    }
+
+    public List<List<LatLng>> getCurrentRoute() {
+        return currentRoute;
+    }
+
+    public Stats getCurrentStats() {
+        return currentStats;
+    }
 }
--- app/src/main/java/com/murraycole/ucrrunner/view/Stats.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/Stats.java
@@ -4,4 +4,45 @@ package com.murraycole.ucrrunner.view;
  * Created by mbrevard on 11/9/14.
  */
 public class Stats {
+    double averageSpeed;
+    double topSpeed;
+    double caloriesBurned;
+    double distance;
+    double elevation;
+
+    Stats() {
+        averageSpeed = -1.0;
+        topSpeed = -1.0;
+        caloriesBurned = -1.0;
+        distance = -1.0;
+        elevation = -1.0;
+    }
+
+    Stats(double averageSpeed, double topSpeed, double caloriesBurned, double distance, double elevation) {
+        this.averageSpeed = averageSpeed;
+        this.topSpeed = topSpeed;
+        this.caloriesBurned = caloriesBurned;
+        this.distance = distance;
+        this.elevation = elevation;
+    }
+
+    public double getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public double getTopSpeed() {
+        return topSpeed;
+    }
+
+    public double getCaloriesBurned() {
+        return caloriesBurned;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public double getElevation() {
+        return elevation;
+    }
 }

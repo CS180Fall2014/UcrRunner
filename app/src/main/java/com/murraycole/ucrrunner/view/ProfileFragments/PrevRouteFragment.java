9fc74f211c16208dea4fbdcd155182620e14846a
ColeMurray <crzdcole@gmail.com>
2014-11-22 01:05:23 -0800

    Prototype of prev routes updated

--- app/src/main/java/com/murraycole/ucrrunner/view/ProfileFragments/PrevRouteFragment.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/ProfileFragments/PrevRouteFragment.java
@@ -6,15 +6,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.murraycole.ucrrunner.R;
+import com.murraycole.ucrrunner.view.Route;
+import com.murraycole.ucrrunner.view.adapters.PrevRouteAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Cole doing this
  */
 public class PrevRouteFragment extends Fragment {
 
-
+    PrevRouteAdapter mAdapter;
     public PrevRouteFragment() {
         // Required empty public constructor
     }
@@ -23,8 +29,21 @@ public class PrevRouteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prev_route, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_prev_route, container, false);
+        ArrayList<Route> routes = spoofData();
+        mAdapter = new PrevRouteAdapter(getActivity(),routes);
+        ListView listView = (ListView) rootview.findViewById(R.id.prev_route_listview);
+        listView.setAdapter(mAdapter);
+
+
+        return rootview;
+    }
+
+    private ArrayList<Route> spoofData(){
+        ArrayList<Route> routeData = new ArrayList<Route>();
+        for (int i = 0; i < 5; i++)
+        routeData.add(new Route());
+        return routeData;
     }
 
 
--- app/src/main/java/com/murraycole/ucrrunner/view/adapters/PrevRouteAdapter.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/adapters/PrevRouteAdapter.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.murraycole.ucrrunner.R;
 import com.murraycole.ucrrunner.view.Route;
 
 import java.util.ArrayList;
@@ -23,13 +26,20 @@ public class PrevRouteAdapter extends ArrayAdapter<Route> {
     public View getView (int position, View convertView, ViewGroup parent){
         //get route
         Route route = getItem(position);
+        View rowView = convertView;
 
         //check if existing view is being reused, otherwise inflate
-        if (convertView == null){
-            //inflate layout of individual listItem.xml
-            //LayoutInflater.from(getContext()).inflate(LAYOUTID,parent,false);
+        if (rowView == null){
+            LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rowView = inflater.inflate(R.layout.route_list_item,parent,false);
+
+            TextView routeDate = (TextView) rowView.findViewById(R.id.route_item_date_textview);
+            TextView routeDist = (TextView) rowView.findViewById(R.id.route_item_dist_textview);
+            ImageView routeImg = (ImageView) rowView.findViewById(R.id.route_item_map_imageview);
+
+
         }
 
-        return convertView;
+        return rowView;
     }
 }

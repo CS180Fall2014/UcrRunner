967a929617e79a38a82f3a5979c24b8889a94625
ColeMurray <crzdcole@gmail.com>
2014-11-22 12:15:19 -0800

    basic message adapter finished

--- app/src/main/java/com/murraycole/ucrrunner/view/ProfileFragments/MailboxFragment.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/ProfileFragments/MailboxFragment.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.murraycole.ucrrunner.R;
+import com.murraycole.ucrrunner.view.adapters.MessagesAdapter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MailboxFragment extends Fragment {
+    MessagesAdapter mAdapter;
+
 
 
     public MailboxFragment() {
@@ -23,8 +29,16 @@ public class MailboxFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mailbox, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_mailbox, container, false);
+        ArrayList<String> messages = new ArrayList<String>();
+        messages.add("random");
+        messages.add("fakeData");
+        mAdapter = new MessagesAdapter(getActivity(),messages);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.message_listview);
+        listView.setAdapter(mAdapter);
+        return rootView;
     }
 
 
--- app/src/main/java/com/murraycole/ucrrunner/view/adapters/MessagesAdapter.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/adapters/MessagesAdapter.java
@@ -1,19 +1,39 @@
 package com.murraycole.ucrrunner.view.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.murraycole.ucrrunner.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by C on 11/22/2014.
  */
 public class MessagesAdapter extends ArrayAdapter {
-    public MessagesAdapter(Context context, int resource) {
-        super(context, resource);
+    public MessagesAdapter(Context context, ArrayList resource) {
+        super(context,0, resource);
     }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View view = convertView;
+
+        if (view == null){
+            LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(R.layout.message_list_item,parent,false);
+            TextView fromField = (TextView) view.findViewById(R.id.message_from_textview);
+            TextView content = (TextView) view.findViewById(R.id.message_content_textview);
+
+            fromField.setText("Mark Wilson");
+            content.setText("This is my message, and I'm exclaiming it to the world! Hear me roar" +
+                    "and triumph");
+
+        }
+        return view;
+
     }
 }

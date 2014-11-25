8720e8aa98b343cbc6475b10b4c8642d29046448
Martin Ting <mting005@ucr.edu>
2014-11-24 17:37:31 -0800

    Add SettingsManager file

---
+++ app/src/main/java/com/murraycole/ucrrunner/backend/SettingsManager.java
@@ -0,0 +1,25 @@
+package com.murraycole.ucrrunner.backend;
+
+import fbutil.firebase4j.service.Firebase;
+
+/**
+ * Created by Martin on 11/24/14.
+ */
+// Wrapper for FirebaseManager::changeSettings() because enums..
+public class SettingsManager {
+    public static void changeNickname(String uid, String nickname){
+        FirebaseManager.changeSetting(FirebaseManager.Setting.NICKNAME, uid, nickname);
+    }
+    public static void changeAge(String uid, int age){
+        FirebaseManager.changeSetting(FirebaseManager.Setting.NICKNAME, uid, age);
+    }
+    public static void changeHeight(String uid, double height){
+        FirebaseManager.changeSetting(FirebaseManager.Setting.HEIGHT, uid, height);
+    }
+    public static void changeWeight(String uid, double weight){
+        FirebaseManager.changeSetting(FirebaseManager.Setting.WEIGHT, uid, weight);
+    }
+    public static void changePassword(String uid, String password){
+        //TODO for dennis to fill out.
+    }
+}

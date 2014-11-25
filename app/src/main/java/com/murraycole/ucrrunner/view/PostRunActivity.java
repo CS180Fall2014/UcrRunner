f4f16ba736647b25ebf6b6d8ccd1571c0fa5dc56
Keshav Dasu <keshavdasu@yahoo.com>
2014-11-24 23:47:07 -0800

    Ask user for Title and Post to Newsfeed
    Added a PopUp window prompting the user to enter a title
    upon hitting ok it will forward the user to the next page asking if
    they want to post it to news feed

--- app/src/main/java/com/murraycole/ucrrunner/view/PostRunActivity.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/PostRunActivity.java
@@ -9,26 +9,24 @@ import android.view.MenuItem;
 import com.murraycole.ucrrunner.R;
 import com.murraycole.ucrrunner.view.ProfileFragments.ProfileFragment;
 
-public class Profile extends Activity {
+public class PostRunActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_post_run);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new ProfileFragment()).commit();
+                    .add(R.id.container, new PostRunFragment()).commit();
 
         }
-
-
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.profile, menu);
+        getMenuInflater().inflate(R.menu.menu_post_run, menu);
         return true;
     }
 
@@ -38,19 +36,12 @@ public class Profile extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
     }
 }

277d719946f083fe54c8d852574465cbe4404e36
Cole Murray <cmurr002@ucr.edu>
2014-11-25 00:49:41 -0800

    added button for new message

--- app/src/main/java/com/murraycole/ucrrunner/view/ProfileFragments/MessageFragments/MailboxFragment.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/ProfileFragments/MessageFragments/MailboxFragment.java
@@ -6,15 +6,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.murraycole.ucrrunner.R;
 import com.murraycole.ucrrunner.backend.FirebaseManager;
 import com.murraycole.ucrrunner.view.DAO.Message;
 import com.murraycole.ucrrunner.view.adapters.MessagesAdapter;
+import com.murraycole.ucrrunner.view.dialogfragments.MessageDialogFragment;
 import com.murraycole.ucrrunner.view.interfaces.ArrayUpdateListener;
 
 import java.util.ArrayList;
+
+import at.markushi.ui.CircleButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +27,7 @@ import java.util.ArrayList;
 public class MailboxFragment extends Fragment implements ArrayUpdateListener{
     MessagesAdapter mAdapter;
     ArrayList<Message> mailbox;
+    CircleButton composeNewButton;
 
 
     public MailboxFragment() {
@@ -35,13 +41,17 @@ public class MailboxFragment extends Fragment implements ArrayUpdateListener{
 
         View rootView = inflater.inflate(R.layout.fragment_mailbox, container, false);
         mailbox = new ArrayList<Message>();
-        /*ArrayList<String> messages = new ArrayList<String>();
-        messages.add("random");
-        messages.add("fakeData");*/
+
         mAdapter = new MessagesAdapter(getActivity(),mailbox);
+        // async updates messages using ArrayUpdateListner.update
         FirebaseManager.getMessages("89",this);
         ListView listView = (ListView) rootView.findViewById(R.id.message_listview);
+        composeNewButton = (CircleButton) rootView.findViewById(R.id.message_compose_new_button);
+        setupComposeNewOnClick(composeNewButton);
+        listView.setClickable(true);
         listView.setAdapter(mAdapter);
+        setupListViewOnItemClick(listView);
+
         return rootView;
     }
 
@@ -50,5 +60,29 @@ public class MailboxFragment extends Fragment implements ArrayUpdateListener{
     public void update(Object o) {
         mailbox.add((Message) o);
         mAdapter.notifyDataSetChanged();
+    }
+
+    private void setupListViewOnItemClick(ListView listView) {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final TextView fromField = (TextView) view.findViewById(R.id.message_from_textview);
+                final TextView content = (TextView) view.findViewById(R.id.message_content_textview);
+                IndividualMessageFragment msgFrag = IndividualMessageFragment.
+                        newInstance(fromField.getText().toString(), content.getText().toString());
+                (getActivity()).getFragmentManager().beginTransaction()
+                        .replace(R.id.container, msgFrag).addToBackStack(null).commit();
+            }
+        });
+    }
+
+    private void setupComposeNewOnClick(CircleButton button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MessageDialogFragment mdf = MessageDialogFragment.newInstance(R.string.message_dialog_title);
+                mdf.show(getFragmentManager(), "messageDialog");
+            }
+        });
     }
 }
--- app/src/main/java/com/murraycole/ucrrunner/view/adapters/MessagesAdapter.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/adapters/MessagesAdapter.java
@@ -1,6 +1,5 @@
 package com.murraycole.ucrrunner.view.adapters;
 
-import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +10,6 @@ import android.widget.TextView;
 import com.murraycole.ucrrunner.R;
 import com.murraycole.ucrrunner.backend.FirebaseManager;
 import com.murraycole.ucrrunner.view.DAO.Message;
-import com.murraycole.ucrrunner.view.ProfileFragments.MessageFragments.IndividualMessageFragment;
 
 import java.util.ArrayList;
 
@@ -20,17 +18,19 @@ import java.util.ArrayList;
  */
 public class MessagesAdapter extends ArrayAdapter {
     ArrayList<Message> mailbox;
+
     public MessagesAdapter(Context context, ArrayList resource) {
-        super(context,0, resource);
+        super(context, 0, resource);
         mailbox = resource;
     }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
 
-        if (view == null){
+        if (view == null) {
             LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = layoutInflater.inflate(R.layout.message_list_item,parent,false);
+            view = layoutInflater.inflate(R.layout.message_list_item, parent, false);
             final TextView fromField = (TextView) view.findViewById(R.id.message_from_textview);
             final TextView content = (TextView) view.findViewById(R.id.message_content_textview);
 
@@ -38,16 +38,6 @@ public class MessagesAdapter extends ArrayAdapter {
 
             fromField.setText(fromNickname);
             content.setText(mailbox.get(position).getMessage());
-
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    IndividualMessageFragment msgFrag = IndividualMessageFragment.
-                            newInstance(fromField.getText().toString(), content.getText().toString());
-                    ((Activity) getContext()).getFragmentManager().beginTransaction()
-                            .replace(R.id.container, msgFrag).addToBackStack(null).commit();
-                }
-            });
 
 
         }

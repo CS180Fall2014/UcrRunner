c804ce3f367c2e572185f3a7738b223b49fe626d
Cole Murray <cmurr002@ucr.edu>
2014-12-01 22:57:17 -0800

    Update to use images in newsfeed. Currently broken

---
+++ app/src/main/java/com/murraycole/ucrrunner/Utils/BitmapToUri.java
@@ -0,0 +1,20 @@
+package com.murraycole.ucrrunner.Utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Created by User on 12/1/14.
+ */
+public class BitmapToUri {
+    public static Uri getImageUriFromBitmap(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 10, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+}

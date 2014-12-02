ffac4542cbe8066022480da3fe811a2617dba5a0
Martin Ting <mting005@ucr.edu>
2014-12-01 18:53:36 -0800

    hella fucking dennis.....

--- app/src/main/java/com/murraycole/ucrrunner/view/adapters/PrevRouteAdapter.java
+++ app/src/main/java/com/murraycole/ucrrunner/view/adapters/PrevRouteAdapter.java
@@ -82,7 +82,7 @@ public class PrevRouteAdapter extends ArrayAdapter<Route> {
 
             String imageArr = FirebaseManager.getImage(title);
             Log.d("MT:", "Got image: " + imageArr);
-            imageArr = imageArr.substring(1, imageArr.length());
+            imageArr = imageArr.substring(0, imageArr.length());
             Log.d("MT", "substrung");
             Log.d("MT", imageArr);
 
